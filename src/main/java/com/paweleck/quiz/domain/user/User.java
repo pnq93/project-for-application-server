90571f1984b7ba03b6ece21a0d79ee5b8ec310b9
Rafał Grzywacz <rafalgrzywacz147@gmail.com>
2018-04-07 12:47:41 +0200

    Implementation of quiz logging from the h2 account

---
+++ src/main/java/com/paweleck/quiz/domain/user/User.java
@@ -0,0 +1,61 @@
+package com.paweleck.quiz.domain.user;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "USER")
+public class User {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 4)
+    private long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "AUTHORITY")
+    private String authority;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
